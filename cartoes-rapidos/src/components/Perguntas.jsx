bde5df19017954558c21d076eca0ee68b1c5bda8
João Lyra <joaolyra24@gmail.com>
2024-03-06 23:11:20 -0300

    Update Perguntas.jsx

--- cartoes-rapidos/src/components/Perguntas.jsx
+++ cartoes-rapidos/src/components/Perguntas.jsx
@@ -1,4 +1,4 @@
-/*import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect } from 'react';
 import api from '../services/api';
 import CardPergunta from './CardPergunta';
 import './Perguntas.css';
@@ -30,53 +30,6 @@ const Perguntas = () => {
       <div className="cards-container">
         {perguntas.map((pergunta) => (
           <CardPergunta key={pergunta.idPergunta} pergunta={pergunta} />
-        ))}
-      </div>
-    </div>
-  );
-};
-
-export default Perguntas; */
-
-import React from 'react';
-import CardPergunta from './CardPergunta';
-import './Perguntas.css';
-
-const Perguntas = () => {
-  // Exemplo de dados fixos
-  const perguntas = [
-    {
-      id: 1,
-      nome: 'Geografia',
-      pergunta: 'Qual é a capital do Brasil?',
-      resposta: 'Brasília',
-    },
-    {
-      id: 2,
-      nome: 'Artes',
-      pergunta: 'Qual é a cor do céu?',
-      resposta: 'Azul',
-    },
-    {
-      id: 2,
-      nome: 'Artes',
-      pergunta: 'Qual é a cor do céu?',
-      resposta: 'Azul',
-    },
-    
-  ];
-
-  return (
-    <div className="perguntas-existentes">
-      <h2>Meus cartões</h2>
-      <div className="cards-container">
-        {perguntas.map((pergunta) => (
-          <CardPergunta
-            key={pergunta.id}
-            nome={pergunta.nome}
-            pergunta={pergunta.pergunta}
-            resposta={pergunta.resposta}
-          />
         ))}
       </div>
     </div>
